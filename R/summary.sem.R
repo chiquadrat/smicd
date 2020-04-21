42c8f07ad84aa955b0349f45d78a7173d369e53a
chiquadrat <chiquadrat@users.noreply.github.com>
2020-04-21 19:16:42 +0200

    Test travis

--- R/summary.sem.R
+++ R/summary.sem.R
@@ -1,4 +1,4 @@
-#' @title Summarizing Linear and Linear Mixed Models estimated with the SEM algorithm
+#' @title Summarizing Linear and Linear Mixed Models estimated with the SEM
 #'
 #' @description \code{summary} method for class \code{"sem"}.
 #' @param object an object of class \code{"sem"}.
